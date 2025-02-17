252aba6e19bb8b53d25279f8c73d2e45631a80f7
aymensellaouti <aymen.sellaouti@gmail.com>
2025-02-17 08:03:52 +0100

    update addCvComponent

--- src/app/cv/add-cv/add-cv.component.ts
+++ src/app/cv/add-cv/add-cv.component.ts
@@ -1,36 +1,91 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { CvService } from '../services/cv.service';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { APP_ROUTES } from 'src/config/routes.config';
-import { Cv } from '../model/cv';
+import { Component, inject, OnDestroy } from "@angular/core";
+import {
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  FormGroup,
+} from "@angular/forms";
+import { CvService } from "../services/cv.service";
+import { Cv } from "../model/cv";
+import { ToastrService } from "ngx-toastr";
+import { Router } from "@angular/router";
+import { APP_ROUTES } from "../../../config/routes.config";
+import { APP_CONSTANTES } from "../../config/app_const.config";
 
 @Component({
-  selector: 'app-add-cv',
-  templateUrl: './add-cv.component.html',
-  styleUrls: ['./add-cv.component.css'],
+  selector: "app-add-cv",
+  templateUrl: "./add-cv.component.html",
+  styleUrls: ["./add-cv.component.css"],
 })
-export class AddCvComponent {
-  constructor(
-    private cvService: CvService,
-    private router: Router,
-    private toaster: ToastrService
-  ) {}
+export class AddCvComponent implements OnDestroy {
+  formBuilder = inject(FormBuilder);
+  cvService = inject(CvService);
+  toastr = inject(ToastrService);
+  router = inject(Router);
 
-  addCv(cv: Cv) {
-    this.cvService.addCv(cv).subscribe({
-      next: () => {
-        this.toaster
-          .success(`Le cv a été ajouté avec succès`);
+  form: FormGroup = this.formBuilder.group(
+    {
+      name: ["", [Validators.required]],
+      firstname: ["", Validators.required],
+      path: [""],
+      job: ["", Validators.required],
+      cin: [
+        "",
+        {
+          validators: [Validators.required, Validators.pattern("[0-9]{8}")],
+          asyncValidators: [],
+        },
+      ],
+      age: [
+        0,
+        {
+          validators: [Validators.required],
+          updateOn: "blur",
+        },
+      ],
+    },
+    {
+      validators: [],
+      asyncValidators: [],
+      updateOn: "change",
+    }
+  );
+  constructor() {}
+
+  addCv() {
+    this.cvService.addCv(this.form.value as Cv).subscribe({
+      next: (cv) => {
+        this.toastr.success(
+          `${cv.firstname} ${cv.name} was added successfully`
+        );
         this.router.navigate([APP_ROUTES.cv]);
       },
-      error: (erreur) => {
-        console.log(erreur);
-        this.toaster.error(
-          `Problème avec le serveur veuillez contacter l'admin`
+      error: (err) => {
+        this.toastr.error(
+          `Un problème au niveau du serveur merci de contacter l'admin`
         );
       },
     });
+  }
+
+  ngOnDestroy(): void {}
+
+  get name(): AbstractControl {
+    return this.form.get("name")!;
+  }
+  get firstname() {
+    return this.form.get("firstname");
+  }
+  get age(): AbstractControl {
+    return this.form.get("age")!;
+  }
+  get job() {
+    return this.form.get("job");
+  }
+  get path() {
+    return this.form.get("path");
+  }
+  get cin(): AbstractControl {
+    return this.form.get("cin")!;
   }
 }
