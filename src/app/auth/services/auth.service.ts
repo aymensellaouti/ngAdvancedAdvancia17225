95817c3edb82a4edb72c45088112c54209eda9b0
aymensellaouti <aymen.sellaouti@gmail.com>
2025-02-19 10:14:54 +0100

    add connectedUser class

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,25 +1,34 @@
-import { Injectable } from '@angular/core';
-import { CredentialsDto } from '../dto/credentials.dto';
-import { LoginResponseDto } from '../dto/login-response.dto';
-import { HttpClient } from '@angular/common/http';
-import { API } from '../../../config/api.config';
-import { Observable } from 'rxjs';
+import { inject, Injectable } from "@angular/core";
+import { CredentialsDto } from "../dto/credentials.dto";
+import { LoginResponseDto } from "../dto/login-response.dto";
+import { HttpClient } from "@angular/common/http";
+import { API } from "../../../config/api.config";
+import { Observable } from "rxjs";
+
+export class ConnectedUser {
+  constructor(public id: number, public email: string) {}
+}
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
-
+  private http = inject(HttpClient);
+  /**
+   * Le flux du user (user / null)
+   */
+  //user$!
+  isLoggedIn$!: Observable<boolean>;
+  isLoggedOut$!: Observable<boolean>;
   login(credentials: CredentialsDto): Observable<LoginResponseDto> {
     return this.http.post<LoginResponseDto>(API.login, credentials);
   }
 
   isAuthenticated(): boolean {
-    return !!localStorage.getItem('token');
+    return !!localStorage.getItem("token");
   }
 
   logout() {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
   }
 }
